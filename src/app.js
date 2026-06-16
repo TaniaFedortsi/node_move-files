7ad91547ff80500f02850560e5d206137e16eeca
TaniaFedortsi <tania.fdrts@gmail.com>
2026-06-16 11:58:40 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,49 @@
-// write code here
+/* eslint-disable no-console */
+const fs = require('fs');
+const path = require('path');
+
+const app = () => {
+  const sourcePath = process.argv[2];
+  const destinationPath = process.argv[3];
+  let finalDestination = destinationPath;
+
+  if (!sourcePath || !destinationPath) {
+    console.error('Missing arguments');
+
+    return;
+  }
+
+  if (sourcePath === destinationPath) {
+    return;
+  }
+
+  if (destinationPath[destinationPath.length - 1] === '/') {
+    if (!fs.existsSync(destinationPath)) {
+      console.error('No such directory');
+
+      return;
+    } else {
+      finalDestination = path.join(destinationPath, path.basename(sourcePath));
+    }
+  } else {
+    if (
+      fs.existsSync(destinationPath) &&
+      fs.statSync(destinationPath).isDirectory()
+    ) {
+      finalDestination = path.join(destinationPath, path.basename(sourcePath));
+    }
+  }
+
+  try {
+    const content = fs.readFileSync(sourcePath);
+
+    fs.writeFileSync(finalDestination, content);
+    fs.unlinkSync(sourcePath);
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+module.exports = { app };
+
+app();
